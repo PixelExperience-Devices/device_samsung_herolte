c8ab69bda9bdd128f668d87b7c785b49668ca50c
Luke Williams <luke@turbolearning.co.uk>
2019-02-13 18:20:26 +0000

    Add Makefiles:
    Pixel Experience
    Clean Open Source Project
    Dirty Unicorns
    Havoc-OS
    ViperOS
    Resurrection Remix
    GZOSP
    Pixel Dust

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,10 @@
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/lineage.mk \
-	$(LOCAL_DIR)/lineage_herolte.mk
+	$(LOCAL_DIR)/lineage_herolte.mk \
+	$(LOCAL_DIR)/aosp_herolte.mk \
+	$(LOCAL_DIR)/cosp_herolte.mk \
+	$(LOCAL_DIR)/du.mk \
+	$(LOCAL_DIR)/gzosp_herolte.mk \
+	$(LOCAL_DIR)/havoc_herolte.mk \
+	$(LOCAL_DIR)/pixeldust_herolte.mk \
+	$(LOCAL_DIR)/rr_herolte.mk \
+	$(LOCAL_DIR)/viper_herolte.mk \
--- aosp_herolte.mk
+++ aosp_herolte.mk
@@ -23,16 +23,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Pixel Experience stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_herolte
+PRODUCT_NAME := aosp_herolte
 PRODUCT_DEVICE := herolte
 PRODUCT_MODEL := SM-G930F
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1440
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=heroltexx \
