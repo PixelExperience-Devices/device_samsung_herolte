ab4fbc7950fa92323135d7ab8c885a1d0abbc2ed
Luke Williams <luke@turbolearning.co.uk>
2019-02-13 19:33:52 +0000

    Update rr_herolte.mk

--- rr_herolte.mk
+++ rr_herolte.mk
@@ -24,7 +24,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit some common Resurrection Remix stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := rr_herolte
