08f181b2246d4d0622be03ad930410e25ee8c176
Luke Williams <lukewilliamsdl@gmail.com>
2019-03-14 01:39:47 +0000

    Preperation for PixelExperience Official

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo lineage_herolte-eng
-add_lunch_combo lineage_herolte-userdebug
-add_lunch_combo lineage_herolte-user
+add_lunch_combo aosp_herolte-eng
+add_lunch_combo aosp_herolte-userdebug
+add_lunch_combo aosp_herolte-user
