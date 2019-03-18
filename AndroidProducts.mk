715dda2d673ba6847dc4deb5871245713741e594
Luke Williams <luke@turbox.uk>
2019-03-18 23:31:30 +0000

    cosp
    cosp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,3 @@
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/lineage_herolte.mk \
 	$(LOCAL_DIR)/aosp_herolte.mk \
 	$(LOCAL_DIR)/cosp_herolte.mk \
-	$(LOCAL_DIR)/du.mk \
-	$(LOCAL_DIR)/gzosp_herolte.mk \
-	$(LOCAL_DIR)/havoc_herolte.mk \
-	$(LOCAL_DIR)/pixeldust_herolte.mk \
-	$(LOCAL_DIR)/rr_herolte.mk \
-	$(LOCAL_DIR)/viper_herolte.mk \
-	$(LOCAL_DIR)/zen_herolte.mk \
-	$(LOCAL_DIR)/revengeos_herolte.mk \
-	$(LOCAL_DIR)/zirconium_herolte.mk \
-
