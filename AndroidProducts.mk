a0ca765070bf07892084d88850a2ba26bd457fc5
ivanmeler <i_ivan@windowslive.com>
2019-09-12 11:23:25 +0000

    Q time

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,6 @@
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/lineage.mk \
 	$(LOCAL_DIR)/lineage_herolte.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_herolte-userdebug
--- vendorsetup.sh
+++
@@ -1,3 +0,0 @@
-add_lunch_combo lineage_herolte-eng
-add_lunch_combo lineage_herolte-userdebug
-add_lunch_combo lineage_herolte-user
